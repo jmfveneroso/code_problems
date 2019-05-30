d36907c8a2725d752fd8c4677b0e09ddf87d94c7
jmfveneroso <jmfveneroso@gmail.com>
2019-05-30 18:10:03 -0300

    Merging sorted files.

---
+++ merging_sorted_files.cpp
@@ -0,0 +1,19 @@
+// =====================================================
+// Book: Elements of programming interviews
+// Problem 10.1: Merging sorted files
+// =====================================================
+
+#include <iostream>
+#include <fstream>
+#include <priority_queue>
+
+// K-way sort just like the one implemented for inverted 
+// index sorting. Uses a heap with at least the size of 
+// the number of files.
+
+using namespace std;
+
+int main() {
+  cout << "Done." << endl;
+  return 0;
+}
