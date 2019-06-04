f0ab89e4d7fadadc3a14dc2140f3d95fb6a43fc1
jmfveneroso <jmfveneroso@gmail.com>
2019-06-04 13:19:48 -0300

    Hash function correction because of overflow.

--- hash_fn_for_words.cpp
+++ hash_fn_for_words.cpp
@@ -10,19 +10,12 @@
 using namespace std;
 
 const int MULTIPLIER = 997;
-const int MOD_INVERSE = 3;
+const int HASH_TABLE_SIZE = 100;
+const int MOD_INVERSE = 333;
 
-int get_mod_inverse(int a, int m) { 
-  a = a % m; 
-  for (int x = 1; x < m; x++) 
-    if ((a * x) % m == 1) 
-      return x; 
-  return -1;
-}
-
-int hash_fn(const string& s, const int& hash_table_size) {
+int hash_fn(const string& s) {
   int res = 0;
-  for (auto& c : s) res = (res * MULTIPLIER + c) % hash_table_size;
+  for (auto& c : s) res = (res * MULTIPLIER + c) % HASH_TABLE_SIZE;
   return res;
 }
 
@@ -30,45 +23,51 @@ int hash_fn(const string& s, const int& hash_table_size) {
 // such that the product ax is congruent to 1 with respect to the 
 // modulus m. We can multiply the hash by this number to obtain a
 // value congruent with the hash divided by the multiplier.
-int replace_first_char(int& hash, string s, char c, const int& hash_table_size) {
+int replace_first_char(int& hash, string s, char c) {
   int pow = 1;
-  for (size_t i = 0; i < s.size() - 1; i++) pow *= MULTIPLIER;
-  hash = (hash - (pow * s[0])) % hash_table_size;
-  hash = (hash + (pow * c)) % hash_table_size;
-  return (hash_table_size + hash) % hash_table_size;
+  for (size_t i = 0; i < s.size() - 1; i++) pow = (pow * MULTIPLIER) % HASH_TABLE_SIZE;
+  hash = (hash - (pow * s[0])) % HASH_TABLE_SIZE;
+  hash = (hash + (pow * c)) % HASH_TABLE_SIZE;
+  return (HASH_TABLE_SIZE + hash) % HASH_TABLE_SIZE;
 }
 
-int add_last_char(int& hash, char c, const int& hash_table_size) {
-  return (hash * MULTIPLIER + c) % hash_table_size;
+int add_last_char(int& hash, char c) {
+  return (hash * MULTIPLIER + c) % HASH_TABLE_SIZE;
 }
 
-int delete_first_char(int hash, string s, const int& hash_table_size) {
+int delete_first_char(int hash, string s) {
   int pow = 1;
-  for (size_t i = 0; i < s.size() - 1; i++) pow *= MULTIPLIER;
-  hash = (hash - (pow * s[0])) % hash_table_size;
-  return (hash_table_size + hash) % hash_table_size;
+  for (size_t i = 0; i < s.size() - 1; i++) pow = (pow * MULTIPLIER) % HASH_TABLE_SIZE;
+
+  hash = (hash - (pow * s[0])) % HASH_TABLE_SIZE;
+  return (HASH_TABLE_SIZE + hash) % HASH_TABLE_SIZE;
 }
 
-int delete_last_char(int hash, string s, const int& hash_table_size) {
-  hash = ((hash - s[s.size() - 1]) * MOD_INVERSE) % hash_table_size;
-  return (hash_table_size + hash) % hash_table_size;
+int delete_last_char(int hash, string s) {
+  hash = ((hash - s[s.size() - 1]) * MOD_INVERSE) % HASH_TABLE_SIZE;
+  return (HASH_TABLE_SIZE + hash) % HASH_TABLE_SIZE;
 }
 
 int main() {
-  cout << "The mod inverse of " << MULTIPLIER << " % 10 is " << get_mod_inverse(MULTIPLIER, 10) << endl;
-
-  int ca_hash = hash_fn("ca", 10);
-  int cat_hash = hash_fn("cat", 10);
-  int at_hash = hash_fn("at", 10);
-  int bat_hash = hash_fn("bat", 10);
+  int ca_hash = hash_fn("ca");
+  int cat_hash = hash_fn("cat");
+  int at_hash = hash_fn("at");
+  int bat_hash = hash_fn("bat");
+  int yielding_hash = hash_fn("yielding");
+  int ielding_hash = hash_fn("ielding");
+  int fielding_hash = hash_fn("fielding");
 
   cout << "cat yields " << cat_hash << endl;
-  cout << "cat yields " << add_last_char(ca_hash, 't', 10) << endl;
+  cout << "cat yields " << add_last_char(ca_hash, 't') << endl;
   cout << "ca yields " << ca_hash << endl;
-  cout << "ca yields " << delete_last_char(cat_hash, "cat", 10) << endl;
+  cout << "ca yields " << delete_last_char(cat_hash, "cat") << endl;
   cout << "at yields " << at_hash << endl;
-  cout << "at yields " << delete_first_char(cat_hash, "cat", 10) << endl;
+  cout << "at yields " << delete_first_char(cat_hash, "cat") << endl;
   cout << "bat yields " << bat_hash << endl;
-  cout << "bat yields " << replace_first_char(cat_hash, "cat", 'b', 10) << endl;
+  cout << "bat yields " << replace_first_char(cat_hash, "cat", 'b') << endl;
+  cout << "ielding yields " << ielding_hash << endl;
+  cout << "ielding yields " << delete_first_char(yielding_hash, "yielding") << endl;
+  cout << "fielding yields " << fielding_hash << endl;
+  cout << "fielding yields " << replace_first_char(yielding_hash, "yielding", 'f') << endl;
   return 0;
 }
