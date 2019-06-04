b9e51fadbc73bfb84379873a7187632ddf5031c6
jmfveneroso <jmfveneroso@gmail.com>
2019-06-04 13:19:32 -0300

    Detect plagiarism.

--- detect_plagiarism.cpp
+++ detect_plagiarism.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <cmath>
 #include <unordered_set>
+#include <vector>
 
 using namespace std;
 
@@ -20,10 +21,11 @@ int hash_fn(const string& s) {
   return res;
 }
 
-int delete_first_char(int hash, string s) {
+int delete_first_char(int hash, string s, size_t i, size_t k) {
   int pow = 1;
-  for (size_t i = 0; i < s.size() - 1; i++) pow *= MULTIPLIER;
-  hash = (hash - (pow * s[0])) % HASH_TABLE_SIZE;
+  for (size_t i = 0; i < k - 1; i++) 
+    pow = (pow * MULTIPLIER) % HASH_TABLE_SIZE;
+  hash = (hash - (pow * s[i])) % HASH_TABLE_SIZE;
   return (HASH_TABLE_SIZE + hash) % HASH_TABLE_SIZE;
 }
 
@@ -32,38 +34,52 @@ int add_char(int& hash, string s, int i) {
   return (HASH_TABLE_SIZE + hash) % HASH_TABLE_SIZE;
 }
 
-// bool compare_strings(string line1, string line2, int k) {
-//   unordered_set<int> substrings;
-// 
-//   int hash = hash_fn(line1.substr(0, k));
-//   for (size_t i = 0; i < line1.size() - k + 1; i++) {
-//     cout << line1.substr(i, k) << endl;
-// 
-//     if (i > 0) {
-//       hash = add_char(hash, line1, i+k);
-//     }
-// 
-//     cout << hash << endl;
-//     if (substrings.find(hash) == substrings.end())
-//       substrings.insert(hash);
-//     hash = delete_first_char(hash, line1, i, k);
-//   }
-// 
-//   cout << "============" << endl;
-//   hash = hash_fn(line2.substr(0, k));
-//   for (size_t i = 0; i < line2.size() - k + 1; i++) {
-//     cout << line2.substr(i, k) << endl;
-// 
-//     if (i > 0) {
-//       hash = add_char(hash, line2, i+k);
-//     }
-// 
-//     cout << hash << endl;
-//     if (substrings.find(hash) != substrings.end()) return true;
-//     hash = delete_first_char(hash, line2, i, k);
-//   }
-//   return false;
-// }
+// This function is not very efficient. An actual hash table 
+// implementation should not use a vector on each key. We could
+// store keys in the next empty slot, and store only the index
+// in the first string instead of the actual key. But this shows
+// how an efficient plagiarism detection method could be 
+// implemented. The time complexity is O(m log(m) + n) where m
+// is the size of the first string and n is the size of the second
+// string.
+bool compare_strings(string line1, string line2, int k) {
+  vector<string> hash_table[1000];
+
+  int hash = hash_fn(line1.substr(0, k));
+  for (size_t i = 0; i < line1.size() - k + 1; i++) {
+    if (i > 0) {
+      hash = delete_first_char(hash, line1, i-1, k);
+      hash = add_char(hash, line1, i+k-1);
+    }
+
+    string s = line1.substr(i, k);
+    if (!hash_table[hash].size()) {
+      hash_table[hash].push_back(s);
+    } else {
+      bool found = false;
+      for (size_t j = 0; j < hash_table[hash].size(); j++) {
+        if (hash_table[hash][j] == s) { found = true; break; }
+      }
+      if (!found) hash_table[hash].push_back(s);
+    }
+  }
+
+  hash = hash_fn(line2.substr(0, k));
+  for (size_t i = 0; i < line2.size() - k + 1; i++) {
+    if (i > 0) {
+      hash = delete_first_char(hash, line2, i-1, k);
+      hash = add_char(hash, line2, i+k-1);
+    }
+
+    if (hash_table[hash].size() > 0) {
+      string s = line2.substr(i, k);
+      for (size_t j = 0; j < hash_table[hash].size(); j++)
+        if (hash_table[hash][j] == s) 
+          return true;
+    }
+  }
+  return false;
+}
 
 int main() {
   string s1 = "pease porridge hot";
@@ -71,19 +87,12 @@ int main() {
   string s3 = "some like it hot";
   string s4 = "some like it cold";
 
-  int h = hash_fn("bato");
-  cout << h << " pease." << endl;
-  int h2 = hash_fn("ato");
-  cout << h2 << " ato" << endl;
-  h = delete_first_char(h, "bato");
-  cout << h << " at" << endl;
-
-  // cout << compare_strings(s1, s2, 5) << " should be true." << endl;
-  // cout << compare_strings(s1, s2, 14) << " should be true." << endl;
-  // cout << compare_strings(s1, s2, 16) << " should be false." << endl;
-  // cout << compare_strings(s1, s3, 3) << " should be true." << endl;
-  // cout << compare_strings(s1, s3, 5) << " should be false." << endl;
-  // cout << compare_strings(s3, s4, 13) << " should be true." << endl;
-  // cout << compare_strings(s3, s4, 14) << " should be false." << endl;
+  cout << compare_strings(s1, s2, 5) << " should be true." << endl;
+  cout << compare_strings(s1, s2, 14) << " should be true." << endl;
+  cout << compare_strings(s1, s2, 16) << " should be false." << endl;
+  cout << compare_strings(s1, s3, 3) << " should be true." << endl;
+  cout << compare_strings(s1, s3, 5) << " should be false." << endl;
+  cout << compare_strings(s3, s4, 13) << " should be true." << endl;
+  cout << compare_strings(s3, s4, 14) << " should be false." << endl;
   return 0;
 }
